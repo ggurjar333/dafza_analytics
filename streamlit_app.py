ae6074bbb4ca3ff56810ab9b181b63632eeda5c7
ggurjar333 <ggurjar333@gmail.com>
2022-11-20 22:44:12 +0530

    added multiselect on countries

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,5 @@ try:
         st.title('Fastest growing industries in Dubai Free Zone - DAFZA as of Today')
 
         st.bar_chart(data=df, x='INDUSTRY NAME', y='NUMBER OF COMPANIES')
-        data_load_state.text('Done!')
+
+data_load_state.text('Done!')
